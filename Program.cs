5ac29705063a909dc48cfd408f48ad248744d350
Daniel Juarez <danieljuarez1996@gmail.com>
2023-12-04 12:54:06 -0300

    commit2

--- Program.cs
+++ Program.cs
@@ -7,18 +7,49 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using tl2_tp10_2023_danielsj1996.Repositorios;
 
-namespace tl2_tp10_2023_danielsj1996
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+var CadenaDeConexion=builder.Configuration.GetConnectionString("SqliteConexion")!.ToString();
+builder.Services.AddSingleton<string>(CadenaDeConexion);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddScoped<IUsuarioRepository,UsuarioRepository>();
+builder.Services.AddScoped<ITableroRepository,TableroRepository>();
+builder.Services.AddScoped<ITareaRepository,TareaRepository>();
+
+builder.Services.AddSession(options=>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+options.IdleTimeout=TimeSpan.FromSeconds(3000);
+options.Cookie.HttpOnly=true;
+options.Cookie.IsEssential=true;
 }
+);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+
+name:"default",
+pattern:"{controller=Home}/{action-Index}/{id?}"
+
+);
+
+app.Run();
+
+
